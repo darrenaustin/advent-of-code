742e55ec140f4bf582eeea8c5b75e94e6b0abd79
Darren Austin <darren@darrenaustin.org>
2024-12-15 13:19:50 -0800

    Cleaned up 2024.15, but made it a bit slower.

--- clojure/src/aoc2024/day15.clj
+++ clojure/src/aoc2024/day15.clj
@@ -14,13 +14,11 @@
    \< dir-left
    \> dir-right})
 
-(defn parse
-  ([input] (parse input identity))
-  ([input grid-fn]
-   (let [[grid-data instructions] (str/split input #"\n\n")
-         grid (parse-grid (grid-fn grid-data))
-         dirs (map robot-dirs (str/replace instructions #"\s" ""))]
-     [grid dirs])))
+(defn parse [input grid-fn]
+  (let [[grid-data instructions] (str/split input #"\n\n")
+        grid (parse-grid (grid-fn grid-data))
+        dirs (map robot-dirs (str/replace instructions #"\s" ""))]
+    [grid dirs]))
 
 (defn double-grid [data]
   (apply str
@@ -31,66 +29,36 @@
                   #"\." ".."
                   #"@"  "@."})))
 
-(def moveable #{\O \@ \[ \]})
-
-(defn space-in-dir [grid loc dir]
-  (let [end (first (drop-while #(moveable (grid %))
-                               (iterate #(vec+ % dir) loc)))]
-    (when (= (grid end) \.)
-      end)))
-
-(defn move [grid from to]
-  (assoc grid to (grid from) from \.))
-
-(defn move-robot [grid dir]
-  (let [robot (loc-where grid #{\@})]
-    (if-let [space (space-in-dir grid robot dir)]
-      (loop [grid grid loc space]
-        (if (= loc robot)
-          grid
-          (let [prev-loc (vec- loc dir)]
-            (recur (move grid prev-loc loc) prev-loc))))
-      grid)))
-
-(defn move-block [grid from to]
-  (move (move grid from to) (vec+ from dir-right) (vec+ to dir-right)))
-
-(defn add-block
-  ([blocks block]
-   (if (some #{block} blocks)
-     blocks
-     (conj blocks block))))
+(defn move-cells [grid locs dir]
+  (let [cells (map grid locs)]
+    (apply assoc grid
+           (concat (mapcat vector locs (repeat \.))
+                   (mapcat vector (map vec+ locs (repeat dir)) cells)))))
 
 (defn moveable-in-dir [grid loc dir]
-  (loop [blocks [] locs [loc]]
-    (if-let [loc (first locs)]
-      (let [next-locs (subvec locs 1)]
+  (loop [blocks #{loc} check [loc]]
+    (if (seq check)
+      (let [loc    (vec+ (first check) dir)
+            check' (subvec check 1)]
         (case (grid loc)
-          \. (recur blocks next-locs)
-          \@ (recur blocks (conj next-locs (vec+ loc dir)))
           \# (recur nil nil)
-          \[ (let [[bx by] loc
-                   next-loc1 (vec+ loc dir)
-                   next-loc2 (vec+ [(inc bx) by] dir)]
-               (recur (add-block blocks loc)
-                      (conj next-locs next-loc1 next-loc2)))
-          \] (let [[bx by] loc
-                   next-loc1 (vec+ loc dir)
-                   next-loc2 (vec+ [(dec bx) by] dir)]
-               (recur (add-block blocks [(dec bx) by])
-                      (conj next-locs next-loc1 next-loc2)))))
+          \. (recur blocks check')
+          \O (recur (conj blocks loc) (conj check' loc))
+          \[ (let [pair-loc (vec+ loc dir-right)]
+               (if (= dir dir-right)
+                 (recur (conj blocks loc pair-loc) (conj check' pair-loc))
+                 (recur (conj blocks loc pair-loc) (conj check' loc pair-loc))))
+          \] (let [pair-loc (vec+ loc dir-left)]
+               (if (= dir dir-left)
+                 (recur (conj blocks loc pair-loc) (conj check' pair-loc))
+                 (recur (conj blocks loc pair-loc) (conj check' loc pair-loc))))))
       blocks)))
 
 (defn move-robot2 [grid dir]
-  (if (zero? (second dir))
-    (move-robot grid dir)
-    (let [robot (loc-where grid #{\@})]
-      (if-let [blocks (moveable-in-dir grid robot dir)]
-        (move (reduce (fn [g b] (move-block g b (vec+ b dir)))
-                      grid
-                      (reverse blocks))
-              robot (vec+ robot dir))
-        grid))))
+  (let [robot (loc-where grid #{\@})]
+    (if-let [blocks (moveable-in-dir grid robot dir)]
+      (move-cells grid blocks dir)
+      grid)))
 
 (defn follow-directions [grid dirs move-fn]
   (reduce (fn [g d] (move-fn g d)) grid dirs))
@@ -98,16 +66,15 @@
 (defn gps-coord [[x y]]
   (+ (* 100 y) x))
 
-;; TODO: clean up and speed up
-(defn part1 [input]
-  (let [[grid dirs] (parse input)]
-    (m/sum (map gps-coord
-                (locs-where (follow-directions grid dirs move-robot)
-                            #{\O})))))
+(defn solve [input box grid-fn]
+  (let [[grid dirs] (parse input grid-fn)]
+    (m/sum
+      (map gps-coord
+           (locs-where (follow-directions grid dirs move-robot2)
+                       #{box})))))
 
-;; TODO: clean up and speed up
-(defn part2 [input]
-  (let [[grid dirs] (parse input double-grid)]
-    (m/sum (map gps-coord
-                (locs-where (follow-directions grid dirs move-robot2)
-                            #{\[})))))
+;; TODO: speed up
+(defn part1 [input] (solve input \O identity))
+
+;; TODO: speed up
+(defn part2 [input] (solve input \[ double-grid))
