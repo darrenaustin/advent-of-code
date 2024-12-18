550c422f22a5a7a9ddf5e20c573d2ae1ba64c554
Darren Austin <darren@darrenaustin.org>
2024-12-17 22:40:54 -0800

    Switched to using sets instead of a map for grids in 2024.18. Didn't have much effect on performance though.

--- clojure/src/aoc2024/day18.clj
+++ clojure/src/aoc2024/day18.clj
@@ -5,6 +5,7 @@
     [aoc.util.grid :refer :all]
     [aoc.util.pathfinding :as p]
     [aoc.util.string :as s]
+    [clojure.set :as set]
     [clojure.string :as str]))
 
 (def input (d/day-input 2024 18))
@@ -15,18 +16,17 @@
 (defn neighbors [grid]
   (fn [loc]
     (init-grid
-      (filter (fn [l] (= \. (grid l))) (orthogonal-from loc))
+      (filter grid (orthogonal-from loc))
       1)))
 
 (defn part1
   ([input] (part1 input 1024 [70 70]))
   ([input bytes goal]
    (let [grid
-         (merge (init-grid (for [x (range (inc (first goal)))
-                                 y (range (inc (second goal)))]
-                             [x y])
-                           \.)
-                (init-grid (take bytes (parse input)) \#))]
+         (set/difference (set (for [x (range (inc (first goal)))
+                                    y (range (inc (second goal)))]
+                                [x y]))
+                         (set (take bytes (parse input))))]
      (p/dijkstra-distance [0 0]
                           (neighbors grid)
                           #{goal}))))
@@ -36,14 +36,13 @@
   ([input] (part2 input 1024 [70 70]))
   ([input skip-bytes goal]
    (let [[pre-drops drops] (split-at skip-bytes (parse input))
-         grid (merge (init-grid (for [x (range (inc (first goal)))
-                                      y (range (inc (second goal)))]
-                                  [x y])
-                                \.)
-                     (init-grid pre-drops \#))]
+         grid (set/difference (set (for [x (range (inc (first goal)))
+                                         y (range (inc (second goal)))]
+                                     [x y]))
+                              (set pre-drops))]
      (loop [grid grid drops drops]
        (when-let [drop (first drops)]
-         (let [grid' (assoc grid drop \#)]
+         (let [grid' (disj grid drop)]
            (if (p/dijkstra-distance [0 0]
                                     (neighbors grid')
                                     #{goal})
