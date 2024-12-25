d0dc6d3ebfc3bcc7f5c4fc33617be520851ca4eb
Darren Austin <darren@darrenaustin.org>
2024-12-24 19:14:02 -0800

    Minor naming cleanup of 2024.24 with some graphviz fun.

--- clojure/src/aoc/util/collection.clj
+++ clojure/src/aoc/util/collection.clj
@@ -4,7 +4,7 @@
   (apply map vector coll))
 
 (defn indexed [coll]
-  (mapv vector (range (count coll)) coll))
+  (map-indexed vector coll))
 
 (defn group-by-value [m]
   (reduce (fn [m [k v]] (update m v conj k)) {} m))
--- clojure/src/aoc2024/day24.clj
+++ clojure/src/aoc2024/day24.clj
@@ -7,8 +7,8 @@
 (defn input [] (d/day-input 2024 24))
 
 (defn parse-gate [input]
-  (let [[w1 op w2 out] (rest (re-find #"(.*) (AND|OR|XOR) (.*) -> (.*)" input))]
-    {:w1 w1 :w2 w2 :op op :out out}))
+  (let [[a op b out] (rest (re-find #"(.*) (AND|OR|XOR) (.*) -> (.*)" input))]
+    {:a a :op op :b b :out out}))
 
 (defn parse [input]
   (let [[init-data conn-data] (str/split input #"\n\n")]
@@ -17,32 +17,32 @@
                   read-string)
      (map parse-gate (str/split-lines conn-data))]))
 
-(defn wires-of [network]
-  (apply set/union (map (fn [g] #{(:w1 g) (:w2 g) (:out g)}) network)))
+(defn wires-of [gates]
+  (apply set/union (map (fn [g] #{(:a g) (:b g) (:out g)}) gates)))
 
 (defn wires->integer [wires]
   (read-string (str "2r" (str/join wires))))
 
-(defn open-gates [values network]
-  (filter #(nil? (values (:out %))) network))
+(defn open-gates [values gates]
+  (filter #(nil? (values (:out %))) gates))
 
 (defn eval-gate [values gate]
-  (let [{:keys [w1 w2 op out]} gate]
+  (let [{:keys [a b op out]} gate]
     {out (case op
-           "AND" (bit-and (values w1) (values w2))
-           "OR" (bit-or (values w1) (values w2))
-           "XOR" (bit-xor (values w1) (values w2)))}))
+           "AND" (bit-and (values a) (values b))
+           "OR" (bit-or (values a) (values b))
+           "XOR" (bit-xor (values a) (values b)))}))
 
-(defn simulate [[initial network]]
-  (let [wires   (wires-of network)
+(defn simulate [[initial-states gates]]
+  (let [wires   (wires-of gates)
         z-wires (reverse (sort (filter #(str/starts-with? % "z") wires)))]
-    (loop [values initial]
+    (loop [values initial-states]
       (let [z-values (map values z-wires)]
         (if (not-any? nil? z-values)
           (wires->integer z-values)
-          (let [gates  (filter #(and (values (:w1 %)) (values (:w2 %)))
-                               (open-gates values network))
-                output (apply merge (map #(eval-gate values %) gates))]
+          (let [has-inputs (filter #(and (values (:a %)) (values (:b %)))
+                                   (open-gates values gates))
+                output     (apply merge (map #(eval-gate values %) has-inputs))]
             (recur (merge values output))))))))
 
 (defn part1 [input]
@@ -58,27 +58,24 @@
 ;;
 ;; TODO: Need to come back and see how much cleaner I can make this.
 ;;
-(defn is-direct? [gate]
-  (or (str/starts-with? (:w1 gate) "x")
-      (str/starts-with? (:w2 gate) "x")))
+(defn direct-gate? [gate]
+  (or (str/starts-with? (:a gate) "x")
+      (str/starts-with? (:b gate) "x")))
 
-(defn is-output? [gate]
+(defn output-gate? [gate]
   (str/starts-with? (:out gate) "z"))
 
-(defn is-type [type]
-  (fn [gate] (= (:op gate) type)))
+(defn has-input? [gate input]
+  (or (= (:a gate) input)
+      (= (:b gate) input)))
 
-(defn has-output [output]
-  (fn [gate] (= (:out gate) output)))
-
-(defn has-input [input]
-  (fn [gate] (or (= (:w1 gate) input)
-                 (= (:w2 gate) input))))
-
-(defn faulty-FAGate0? [{:keys [w1 w2 out] :as gate}]
-  (let [first? (or (= w1 "x00") (= w2 "x00"))]
+(defn faulty-gate0? [{:keys [a b out] :as gate}]
+  (let [first? (or (= a "x00") (= b "x00"))]
     (or (and first? (not= out "z00"))
-        (and (not first?) (is-output? gate)))))
+        (and (not first?) (output-gate? gate)))))
+
+(defn faulty-gate3? [gate]
+  (not (output-gate? gate)))
 
 (defn faulty-output? [gate]
   ;; TODO: shouldn't hard code this for z45
@@ -93,20 +90,20 @@
       (let [{:keys [out] :as g0} (first g0s)]
         (if (or (faulty g0) (= out "z00"))
           (recur (rest g0s) faulty' check-gates)
-          (let [matches (filter #((has-input (:out g0)) %) g3s)]
+          (let [matches (filter #(has-input? % (:out g0)) g3s)]
             (if (empty? matches)
               (recur (rest g0s) (conj faulty' (:out g0)) (conj check-gates g0))
               (recur (rest g0s) faulty' check-gates))))))))
 
-(defn determine-flagged-gates [gates check-gates g3s faulty]
+(defn determine-flagged-expected [gates check-gates g3s faulty]
   (loop [check-gates check-gates faulty' faulty]
     (if (empty? check-gates)
       faulty'
-      (let [{:keys [w1]} (first check-gates)
-            intended       (str "z" (subs w1 1))
-            matches        (filter (has-output intended) g3s)
-            inputs         #{(:w1 (first matches)) (:w2 (first matches))}
-            or-matches     (filter #(and ((is-type "OR") %)
+      (let [{:keys [a]} (first check-gates)
+            intended       (str "z" (subs a 1))
+            matches        (filter #(= (:out %) intended) g3s)
+            inputs         #{(:a (first matches)) (:b (first matches))}
+            or-matches     (filter #(and (= "OR" (:op %))
                                          (inputs (:out %))) gates)
             or-output      (:out (first or-matches))
             correct-output (first (set/difference inputs #{or-output}))]
@@ -114,12 +111,60 @@
 
 (defn part2 [input]
   (let [[_ gates] (parse input)
-        g0s     (filter #(and (is-direct? %) ((is-type "XOR") %)) gates)
-        g3s     (filter #(and (not (is-direct? %)) ((is-type "XOR") %)) gates)
-        outputs (filter is-output? gates)
-        faulty  (set (map :out (concat (filter faulty-FAGate0? g0s)
-                                       (filter #(not (is-output? %)) g3s)
+        g0s     (filter #(and (direct-gate? %) (= "XOR" (:op %))) gates)
+        g3s     (filter #(and (not (direct-gate? %)) (= "XOR" (:op %))) gates)
+        outputs (filter output-gate? gates)
+        faulty  (set (map :out (concat (filter faulty-gate0? g0s)
+                                       (filter faulty-gate3? g3s)
                                        (filter faulty-output? outputs))))
         [check-gates faulty] (faulty-0-3-connections g0s g3s faulty)
-        faulty  (determine-flagged-gates gates check-gates g3s faulty)]
+        faulty  (determine-flagged-expected gates check-gates g3s faulty)]
     (str/join "," (sort faulty))))
+
+(def faulty ["drg" "gvw" "jbp" "jgc" "qjb" "z15" "z22" "z35"])
+
+(defn graph-vis [gates faulty]
+  (let [inputs    (sort (set (mapcat (fn [g] [(:a g) (:b g)]) gates)))
+        outputs   (sort (set (map :out gates)))
+        xs        (filter #(str/starts-with? % "x") inputs)
+        ys        (filter #(str/starts-with? % "y") inputs)
+        zs        (filter #(str/starts-with? % "z") outputs)
+        or-gates  (map-indexed (fn [i g] (assoc g :op (format "OR%02d" i)))
+                               (filter #(= "OR" (:op %)) gates))
+        xor-gates (map-indexed (fn [i g] (assoc g :op (format "XOR%02d" i)))
+                               (filter #(= "XOR" (:op %)) gates))
+        and-gates (map-indexed (fn [i g] (assoc g :op (format "AND%02d" i)))
+                               (filter #(= "AND" (:op %)) gates))
+        gates     (concat or-gates xor-gates and-gates)
+        ors       (map :op or-gates)
+        xors      (map :op xor-gates)
+        ands      (map :op and-gates)]
+    (println "strict digraph {")
+    (println "  graph [")
+    (println "    label = \"\\n\\nAdvent of Code 2024 day 24\"")
+    (println "    fontsize=20")
+    (println "  ]")
+    (println "  subgraph {")
+    (println "    rank=min")
+    (println "    node[style=filled;color=\"#aaf\"];")
+    (println "   " (str/join "; " (concat xs ys)))
+    (println "  }")
+    (println "  subgraph {")
+    (println "    rank=max")
+    (println "    node[style=filled;color=\"#afa\"];")
+    (println "   " (str/join "; " zs))
+    (println "  }")
+    (println "  {")
+    (println "    node[shape=rect;style=filled;color=lightgray];")
+    (println "   " (str/join "; " (concat ands ors xors)))
+    (println "  }")
+    (println "  {")
+    (println "    node[style=filled;color=red;pencolor=white];")
+    (println "   " (str/join "; " faulty))
+    (println "  }")
+    (doseq [gate gates]
+      (println " " (format "%s -> %s" (:a gate) (:op gate)))
+      (println " " (format "%s -> %s" (:b gate) (:op gate)))
+      (println " " (format "%s -> %s" (:op gate) (:out gate)))
+      (println))
+    (println "}")))
