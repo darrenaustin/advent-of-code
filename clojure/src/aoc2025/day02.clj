25caf6b5cd5220a3737eb1852c60acec6b544ce4
Darren Austin <darren@darrenaustin.org>
2025-12-01 22:04:40 -0800

    Minor cleanup of 2025.02

--- clojure/src/aoc2025/day02.clj
+++ clojure/src/aoc2025/day02.clj
@@ -8,16 +8,20 @@
 (defn input [] (d/day-input 2025 2))
 
 (defn parse-ranges [input]
-  (map (fn [[_ start end]] [(s/parse-int start) (s/parse-int end)]) (re-seq #"(\d+)-(\d+)" input)))
+  (map (fn [[_ start end]] [(s/parse-int start) (s/parse-int end)])
+       (re-seq #"(\d+)-(\d+)" input)))
 
-(defn single-repeats-in-range [[start end]]
-  (filter #(re-matches #"(.*)(\1)" (str %)) (range start (inc end))))
+(defn matches-in-range [regex [start end]]
+  (filter #(re-matches regex (str %)) (range start (inc end))))
 
-(defn multiple-repeats-in-range [[start end]]
-  (filter #(re-matches #"(.*)((\1)+)" (str %)) (range start (inc end))))
+(defn single-repeats-in-range [num-range]
+  (matches-in-range #"^(.+)\1$" num-range))
+
+(defn multiple-repeats-in-range [num-range]
+  (matches-in-range #"^(.+)\1+$"  num-range))
 
 (defn part1 [input]
-  (m/sum (flatten (map single-repeats-in-range (parse-ranges input)))))
+  (m/sum (mapcat single-repeats-in-range (parse-ranges input))))
 
 (defn part2 [input]
-  (m/sum (flatten (map multiple-repeats-in-range (parse-ranges input)))))
+  (m/sum (mapcat multiple-repeats-in-range (parse-ranges input))))
