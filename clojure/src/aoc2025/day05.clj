8a12a8e2229b201dd1b69b595448908dfc2ed0c9
Darren Austin <darren@darrenaustin.org>
2025-12-04 21:56:29 -0800

    Solved 2025.05

---
+++ clojure/src/aoc2025/day05.clj
@@ -0,0 +1,41 @@
+;; https://adventofcode.com/2025/day/5
+ (ns aoc2025.day05
+   (:require
+    [aoc.day :as d]
+    [aoc.util.math :as m]
+    [aoc.util.string :as s]
+    [clojure.string :as str]))
+
+(defn input [] (d/day-input 2025 5))
+
+(defn parse-ingredients [input]
+  (let [[ranges ingredients] (s/split-blocks input)
+        ranges (map s/parse-pos-ints (str/split-lines ranges))
+        ingredients (s/parse-ints ingredients)]
+    [ranges ingredients]))
+
+(defn in-range? [[start end] ingredient]
+  (and (<= start ingredient) (>= end ingredient)))
+
+(defn in-ranges? [rs ingredient]
+  (some #(in-range? % ingredient) rs))
+
+(defn collapse-ranges [rs]
+  (loop [current (first rs) collapsed [] [next & rest] (rest rs)]
+    (if (nil? next)
+      (conj collapsed current)
+      (let [[cs ce] current [ns ne] next]
+        (if (<= ns ce)
+          (recur [cs (max ce ne)] collapsed rest)
+          (recur next (conj collapsed current) rest))))))
+
+(defn range-size [[start end]]
+  (inc (- end start)))
+
+(defn part1 [input]
+  (let [[ranges ingredients] (parse-ingredients input)]
+    (count (filter #(in-ranges? ranges %) ingredients))))
+
+(defn part2 [input]
+  (let [[ranges _] (parse-ingredients input)]
+    (m/sum (map range-size (collapse-ranges (sort-by first ranges))))))
---
+++ clojure/test/aoc2025/day05_test.clj
@@ -0,0 +1,33 @@
+;; Test for aoc2025.day5
+(ns aoc2025.day05-test
+  (:require [aoc.day :refer [day-answers]]
+            [aoc2025.day05 :as d]
+            [clojure.test :refer :all]))
+
+(def example
+  "3-5
+10-14
+16-20
+12-18
+
+1
+5
+8
+11
+17
+32")
+
+(deftest part1-example
+  (is (= 3 (d/part1 example))))
+
+(deftest part2-example
+  (is (= 14 (d/part2 example))))
+
+(def answers (delay (day-answers 2025 5)))
+(def input (delay (d/input)))
+
+(deftest part1-correct
+  (is (= (:answer1 @answers) (d/part1 @input))))
+
+(deftest part2-correct
+  (is (= (:answer2 @answers) (d/part2 @input))))
