6456fbd3a3d9cf455099ba435ec3ab0320542ae9
Darren Austin <darren@darrenaustin.org>
2025-12-11 01:26:38 -0800

    Fixed the clj-kondo hook and cleaned up 2025.11

--- clojure/.clj-kondo/hooks/aoc/util/memoize.clj
+++ clojure/.clj-kondo/hooks/aoc/util/memoize.clj
@@ -13,28 +13,28 @@
           (let [second-child (second fn-children)]
             (if (api/vector-node? second-child)
               ;; (fn [args] ...) -> (let [name (fn name [args] ...)] ...)
-              (let [new-fn-name (api/token-node (:string-value fn-name))
+              (let [new-fn-name (api/token-node (symbol (:string-value fn-name)))
                     new-fn-def (api/list-node
                                 (list* first-child       ;; fn
                                        new-fn-name       ;; name
                                        (rest fn-children))) ;; [args] body...
                     new-node (api/list-node
-                              (list* (api/token-node "let")
+                              (list* (api/token-node 'let)
                                      (api/vector-node [fn-name new-fn-def])
                                      body))]
                 {:node new-node})
               ;; (fn name [args] ...) -> leave as is, but wrap in let
               {:node (api/list-node
-                      (list* (api/token-node "let")
+                      (list* (api/token-node 'let)
                              bindings
                              body))}))
           ;; Not a fn form, just wrap in let
           {:node (api/list-node
-                  (list* (api/token-node "let")
+                  (list* (api/token-node 'let)
                          bindings
                          body))}))
       ;; Fallback
       {:node (api/list-node
-              (list* (api/token-node "let")
+              (list* (api/token-node 'let)
                      bindings
                      body))})))
--- clojure/src/aoc2025/day11.clj
+++ clojure/src/aoc2025/day11.clj
@@ -13,27 +13,23 @@
                    [name connections])
                 (str/split-lines input))))
 
-(defn count-paths [start finish devices]
-  (let-memoized
-   [num-paths (fn [start]
-                (if (= start finish)
-                  1
-                  (let [result (mapv num-paths (devices start))]
-                    (m/sum result))))]
-   (num-paths start)))
-
-(defn count-paths-passing-through [start finish points-of-interest devices]
-  (let-memoized
-   [num-paths (fn [start seen]
-                (if (= start finish)
-                  (if (= seen points-of-interest) 1 0)
-                  (let [seen' (if (points-of-interest start) (conj seen start) seen)
-                        result (mapv #(num-paths % seen') (devices start))]
-                    (m/sum result))))]
-   (num-paths start #{})))
+(defn count-paths
+  ([start finish devices] (count-paths start finish nil devices))
+  ([start finish mandatory devices]
+   (let-memoized
+    [num-paths (fn [start seen]
+                 (if (= start finish)
+                   (if mandatory
+                     (if (= seen mandatory) 1 0)
+                     1)
+                   (let [seen' (when mandatory
+                                 (if (mandatory start) (conj seen start) seen))
+                         result (mapv #(num-paths % seen') (devices start))]
+                     (m/sum result))))]
+    (num-paths start (when mandatory #{})))))
 
 (defn part1 [input]
   (count-paths "you" "out" (parse-devices input)))
 
 (defn part2 [input]
-  (count-paths-passing-through "svr" "out" #{"dac" "fft"} (parse-devices input)))
+  (count-paths "svr" "out" #{"dac" "fft"} (parse-devices input)))
