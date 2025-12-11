ff166f1e19a530516665d2cecd9baaf8986b33d0
Darren Austin <darren@darrenaustin.org>
2025-12-10 16:55:58 -0800

    Solved 2025.10 p1. P2 requires a solver or something.

--- clojure/src/aoc2025/day10.clj
+++ clojure/src/aoc2025/day10.clj
@@ -1,19 +1,81 @@
 ;; https://adventofcode.com/2025/day/10
-(ns aoc2025.day10
-  (:require
-   [aoc.day :as d]
-   [aoc.util.collection :as c]
-   [aoc.util.math :as m]
-   [aoc.util.grid2 :as g]
-   [aoc.util.vec :as v]
-   [aoc.util.string :as s]
-   [clojure.string :as str]))
+ (ns aoc2025.day10
+   (:require
+    [aoc.day :as d]
+    [aoc.util.math :as m]
+    [aoc.util.string :as s]
+    [clojure.math :refer [pow]]
+    [clojure.string :as str])
+   (:import
+    [clojure.lang PersistentQueue]))
 
 (defn input [] (d/day-input 2025 10))
 
-(defn part1 [input])
+(defn parse-machines [input]
+  (map (fn [line]
+         (let [[[_ lights-str buttons-str _]] (re-seq #"^\[(.*?)\]\s*((?:\([^)]+\)\s*)+)\s*\{(.*)\}$" line)
+               lights (read-string (str "2r" (str/join (mapv #(if (= \# %) 1 0) lights-str))))
+               buttons (mapv (fn [b] (m/sum (map #(int (pow 2 (- (dec (count lights-str)) %))) (s/parse-ints b))))
+                             (str/split buttons-str #" "))]
+           {:lights-goal lights, :buttons buttons})) (str/split-lines input)))
 
-(defn part2 [input])
 
-(def t
-  "")
+(defn min-presses [{:keys [lights-goal buttons] :as machine}]
+  (loop [open (conj PersistentQueue/EMPTY [0 0]), visited #{0}]
+    (when-let [[state depth] (peek open)]
+      (if (= state lights-goal)
+        depth
+        (let [[open' visited']
+              (reduce (fn [[o v] b] (let [new-state (bit-xor state b)]
+                                      (if-not (v new-state)
+                                        [(conj o [new-state (inc depth)]) (conj v new-state)]
+                                        [o v])))
+                      [(pop open) visited]
+                      buttons)]
+          (recur open' visited'))))))
+
+(defn part1 [input]
+  (->> input
+       parse-machines
+       (map min-presses)
+       m/sum))
+
+(defn parse-machines2 [input]
+  (map (fn [line]
+         (let [[[_ _ buttons-str joltage-str]] (re-seq #"^\[(.*?)\]\s*((?:\([^)]+\)\s*)+)\s*\{(.*)\}$" line)
+               joltage-goal (s/parse-ints joltage-str)
+               buttons (mapv s/parse-ints (str/split buttons-str #" "))]
+           {:joltage-goal joltage-goal, :buttons buttons}))
+       (str/split-lines input)))
+
+
+(defn press-button [joltages button]
+  (reduce (fn [js c] (update js c inc)) joltages button))
+
+(defn under-joltage? [goal joltage]
+  (every? #(<= (nth joltage %) (nth goal %)) (range (count goal))))
+
+(defn min-presses2 [{:keys [joltage-goal buttons] :as machine}]
+  (let [starting (vec (repeat (count joltage-goal) 0))]
+    (loop [open (conj PersistentQueue/EMPTY [starting 0]), visited #{starting}]
+      (when-let [[state depth] (peek open)]
+        (if (= state joltage-goal)
+          depth
+          (let [[open' visited']
+                (reduce (fn [[o v] b] (let [new-state (press-button state b)]
+                                        (if (and (not (v new-state)) (under-joltage? joltage-goal new-state))
+                                          [(conj o [new-state (inc depth)]) (conj v new-state)]
+                                          [o v])))
+                        [(pop open) visited]
+                        buttons)]
+            (recur open' visited')))))))
+
+;; TODO: this doesn't work for the real input as it is way too slow. Need
+;; to look into some kind of linear solver or something.
+
+;; TODO: also need to cleanup the code duplication above
+(defn part2 [input]
+  (->> input
+       parse-machines2
+       (map min-presses2)
+       m/sum))
--- clojure/test/aoc2025/day10_test.clj
+++ clojure/test/aoc2025/day10_test.clj
@@ -5,23 +5,15 @@
             [clojure.test :refer :all]))
 
 (def example
-  "")
+  "[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
+[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}")
 
 (deftest part1-example
-  (is (= nil (d/part1 example))))
-
-(deftest part1-examples
-  (are [expected input]
-    (= expected (d/part1 input))
-    nil ""))
+  (is (= 7 (d/part1 example))))
 
 (deftest part2-example
-  (is (= nil (d/part2 example))))
-
-(deftest part2-examples
-  (are [expected input]
-    (= expected (d/part2 input))
-    nil ""))
+  (is (= 33 (d/part2 example))))
 
 (def answers (delay (day-answers 2025 10)))
 (def input (delay (d/input)))
@@ -29,5 +21,5 @@
 (deftest part1-correct
   (is (= (:answer1 @answers) (d/part1 @input))))
 
-(deftest part2-correct
+(deftest ^:slow part2-correct
   (is (= (:answer2 @answers) (d/part2 @input))))
