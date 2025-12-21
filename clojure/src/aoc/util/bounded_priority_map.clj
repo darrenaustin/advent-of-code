6c8d58da018a0dde91ad80e4fa7784bfdffe6a33
Darren Austin <darren@darrenaustin.org>
2025-12-20 16:49:48 -0800

    Updated the splint rules for deftype files to make them less verbose.

--- clojure/src/aoc/util/bounded_priority_map.clj
+++ clojure/src/aoc/util/bounded_priority_map.clj
@@ -14,8 +14,6 @@
     Seqable
     Sorted]))
 
-#_{:splint/disable [lint lint/prefer-method-values]}
-
 (deftype BoundedPriorityMap [pm bound]
   IPersistentMap
   (assoc [this k v]
@@ -24,9 +22,9 @@
       (if (or (nil? bound) (< (count pm) bound))
         (BoundedPriorityMap. (assoc pm k v) bound)
         (let [last-entry (first (rseq pm))
-              comp (Sorted/.comparator ^Sorted pm)
+              comp (.comparator ^Sorted pm)
               should-add? (if comp
-                            (neg? (java.util.Comparator/.compare comp v (val last-entry)))
+                            (neg? (.compare comp v (val last-entry)))
                             (neg? (compare v (val last-entry))))]
           (if should-add?
             (BoundedPriorityMap. (-> pm (dissoc (key last-entry)) (assoc k v)) bound)
@@ -34,7 +32,7 @@
   (assocEx [this k v]
     (if (contains? pm k)
       (throw (Exception. "Key already present"))
-      (IPersistentMap/.assoc this k v)))
+      (.assoc this k v)))
   (without [this k]
     (BoundedPriorityMap. (dissoc pm k) bound))
 
@@ -42,32 +40,32 @@
   (count [this] (count pm))
   (cons [this o]
     (if (map? o)
-      (reduce (fn [m [k v]] (IPersistentMap/.assoc m k v)) this o)
+      (reduce (fn [m [k v]] (.assoc m k v)) this o)
       (if (vector? o)
-        (IPersistentMap/.assoc this (nth o 0) (nth o 1))
+        (.assoc this (nth o 0) (nth o 1))
         (throw (Exception. "Don't know how to cons to BoundedPriorityMap")))))
   (empty [this] (BoundedPriorityMap. (empty pm) bound))
   (equiv [this o]
     (and (instance? BoundedPriorityMap o)
          (= bound (.bound ^BoundedPriorityMap o))
-         (IPersistentMap/.equiv pm (.pm ^BoundedPriorityMap o))))
+         (.equiv pm (.pm ^BoundedPriorityMap o))))
 
   Seqable
   (seq [this] (seq pm))
 
   ILookup
-  (valAt [this k] (ILookup/.valAt pm k))
-  (valAt [this k not-found] (ILookup/.valAt pm k not-found))
+  (valAt [this k] (.valAt pm k))
+  (valAt [this k not-found] (.valAt pm k not-found))
 
   Associative
-  (containsKey [this k] (Associative/.containsKey pm k))
-  (entryAt [this k] (Associative/.entryAt pm k))
+  (containsKey [this k] (.containsKey pm k))
+  (entryAt [this k] (.entryAt pm k))
 
   Sorted
-  (comparator [this] (Sorted/.comparator ^Sorted pm))
-  (entryKey [this k] (Sorted/.entryKey ^Sorted pm k))
-  (seq [this ascending] (Sorted/.seq ^Sorted pm ascending))
-  (seqFrom [this k ascending] (Sorted/.seqFrom ^Sorted pm k ascending))
+  (comparator [this] (.comparator ^Sorted pm))
+  (entryKey [this k] (.entryKey ^Sorted pm k))
+  (seq [this ascending] (.seq ^Sorted pm ascending))
+  (seqFrom [this k ascending] (.seqFrom ^Sorted pm k ascending))
 
   MapEquivalence
 
@@ -83,11 +81,11 @@
   (toString [this] (str pm))
 
   IFn
-  (invoke [this k] (ILookup/.valAt pm k))
-  (invoke [this k not-found] (ILookup/.valAt pm k not-found))
+  (invoke [this k] (.valAt pm k))
+  (invoke [this k not-found] (.valAt pm k not-found))
 
   java.lang.Iterable
-  (iterator [this] (java.lang.Iterable/.iterator ^java.lang.Iterable pm)))
+  (iterator [this] (.iterator ^java.lang.Iterable pm)))
 
 (defn bounded-priority-map
   "Creates a new bounded priority map with the given capacity bound.
@@ -112,4 +110,4 @@
 (defmethod print-method BoundedPriorityMap
   print-method-bounded-priority-map
   [o w]
-  (java.io.Writer/.write w (str o)))
+  (.write w (str o)))
