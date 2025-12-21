e13e521f757c4c2ce7507141041387308d38761a
Darren Austin <darren@darrenaustin.org>
2025-12-21 00:32:44 -0800

    Converted 2017.14 over to using sparse-grid. Also changed to a faster connected-components approach.

--- clojure/src/aoc2017/day14.clj
+++ clojure/src/aoc2017/day14.clj
@@ -3,42 +3,46 @@
   (:require
    [aoc.day :as d]
    [aoc.util.collection :refer [count-where]]
-   [aoc.util.grid :as g]
    [aoc.util.pos :as p]
+   [aoc.util.sparse-grid :as sg]
    [aoc2017.knot-hash :refer [knot-hash]]
-   [clojure.pprint :refer [cl-format]]
+   [clojure.set :as set]
    [clojure.string :as str]))
 
 (defn input [] (d/day-input 2017 14))
 
+(def hex-map
+  {\0 "0000" \1 "0001" \2 "0010" \3 "0011"
+   \4 "0100" \5 "0101" \6 "0110" \7 "0111"
+   \8 "1000" \9 "1001" \a "1010" \b "1011"
+   \c "1100" \d "1101" \e "1110" \f "1111"})
+
 (defn binary [hex]
-  (str/join (map #(cl-format nil "~16,'0b"
-                             (read-string (str "16r" (str/join %))))
-                 (partition 4 hex))))
+  (str/join (map hex-map hex)))
 
 (defn knot-hashs [input]
-  (map #(binary (knot-hash (str input "-" %))) (range 128)))
+  (mapv #(binary (knot-hash (str input "-" %))) (range 128)))
 
-(defn region-around [loc adjacent-fn]
-  (loop [[l & ls] #{loc} region #{}]
-    (if (nil? l)
-      region
-      (let [neighbors (remove region (adjacent-fn l))]
-        (recur (set (concat ls neighbors)) (conj region l))))))
-
-(defn flood-fill [seeds adjacent-fn]
-  (loop [[s & seeds] (set seeds) regions []]
-    (if (nil? s)
-      regions
-      (let [region (region-around s adjacent-fn)]
-        (recur (apply disj (set seeds) region) (conj regions region))))))
-
-(defn neighbors-fn [grid]
-  (fn [loc]
-    (filter #(= \1 (grid %)) (p/orthogonal-to loc))))
+(defn connected-components [nodes neighbors-fn]
+  (loop [nodes (set nodes)
+         components []]
+    (if (empty? nodes)
+      components
+      (let [root (first nodes)
+            ;; BFS to find the component
+            component (loop [q [root] visited #{root}]
+                        (if (empty? q)
+                          visited
+                          (let [n (peek q)
+                                nbrs (filter nodes (neighbors-fn n))
+                                new-nbrs (remove visited nbrs)]
+                            (recur (into (pop q) new-nbrs)
+                                   (into visited new-nbrs)))))]
+        (recur (set/difference nodes component)
+               (conj components component))))))
 
 (defn hash-grid [input]
-  (g/parse-grid (str/join "\n" (knot-hashs input))))
+  (sg/rows->sparse-grid (knot-hashs input) #(when (= \1 %) true)))
 
 (defn part1 [input]
   (reduce (fn [s row] (+ s (count-where #{\1} row)))
@@ -47,4 +51,4 @@
 
 (defn part2 [input]
   (let [grid (hash-grid input)]
-    (count (flood-fill (g/locs-where grid #{\1}) (neighbors-fn grid)))))
+    (count (connected-components (keys grid) p/orthogonal-to))))
