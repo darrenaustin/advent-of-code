16c557cd9aa85c37b538a84c960228536d7640c9
Darren Austin <darren@darrenaustin.org>
2026-01-02 15:46:34 -0800

    Added a compile step to the assembunny interpreter to speed it up some more. Fun!

--- clojure/src/aoc2016/assembunny.clj
+++ clojure/src/aoc2016/assembunny.clj
@@ -9,18 +9,9 @@
      (or (s/int x) (keyword x))
      (or (s/int y) (keyword y))]))
 
-(defn init-program
-  ([input] (init-program input {}))
-  ([input registers]
-   {:pc 0
-    :code (mapv parse-instruction (s/lines input))
-    :regs (merge {:a 0, :b 0, :c 0, :d 0} registers)
-    :output []
-    :done false}))
-
-(defn- toggle [pc program]
-  (if-let [[op x y] (get program pc)]
-    (assoc program pc
+(defn- toggle [pc code]
+  (if-let [[op x y] (get code pc)]
+    (assoc code pc
            (case op
              :cpy [:jnz x y]
              :inc [:dec x y]
@@ -28,7 +19,7 @@
              :jnz [:cpy x y]
              :tgl [:inc x y]
              :out [:inc x y]))
-    program))
+    code))
 
 (defn- multiplication [pc program]
   ;; Look for something like:
@@ -54,7 +45,7 @@
                 c (get-in block [4 1])]
             (when (and (= b (get-in block [2 1]) (get-in block [3 1]))
                        (= c (get-in block [4 1]) (get-in block [5 1])))
-              [m a b c])))))))
+              [[:mul m a b c] 6])))))))
 
 (defn- addition [pc program]
   ;; inc x; dec y; jnz y -2
@@ -67,12 +58,12 @@
           (and (= (mapv first block) [:inc :dec :jnz])
                (= -2 (get-in block [2 2]))
                (= (get-in block [1 1]) (get-in block [2 1])))
-          [(get-in block [0 1]) (get-in block [1 1])]
+          [[:add (get-in block [0 1]) (get-in block [1 1])] 3]
 
           (and (= (mapv first block) [:dec :inc :jnz])
                (= -2 (get-in block [2 2]))
                (= (get-in block [0 1]) (get-in block [2 1])))
-          [(get-in block [1 1]) (get-in block [0 1])])))))
+          [[:add (get-in block [1 1]) (get-in block [0 1])] 3])))))
 
 (defn- division [pc program]
   ;; 0: cpy 2 c
@@ -97,67 +88,87 @@
                 a (get-in block [6 1])]
             (when (and (= b (get-in block [3 1]))
                        (= c (get-in block [4 1]) (get-in block [5 1])))
-              [a b c])))))))
+              [[:div a b c] 8])))))))
 
-(defn- value [program x]
-  (get (:regs program) x x))
+(defn- optimize [code]
+  (loop [pc 0, opt-code []]
+    (if (>= pc (count code))
+      opt-code
+      (if-let [[inst len] (or (division pc code)
+                              (multiplication pc code)
+                              (addition pc code))]
+        (recur (+ pc len) (into opt-code (cons inst (repeat (dec len) [:nop]))))
+        (recur (inc pc) (conj opt-code (code pc)))))))
+
+(defn init-program
+  ([input] (init-program input {}))
+  ([input registers]
+   (let [code (mapv parse-instruction (s/lines input))]
+     {:pc 0
+      :code code
+      :opt-code (optimize code)
+      :regs (merge {:a 0, :b 0, :c 0, :d 0} registers)
+      :output []
+      :done false})))
 
 (defn step [program]
-  (let [{:keys [pc code regs]} program]
-    (if (>= pc (count code))
+  (let [{:keys [pc opt-code regs]} program]
+    (if (>= pc (count opt-code))
       (assoc program :done true)
-      (if-let [[a b c] (division pc code)]
-        (if (pos? (value program b))
-          (-> program
-              (update :pc + 8)
-              (assoc :regs (-> regs
-                               (update a + (quot (value program b) 2))
-                               (assoc c (- 2 (mod (value program b) 2)))
-                               (assoc b 0))))
-          (update program :pc inc))
-        (if-let [[m a b c] (multiplication pc code)]
-          (if (and (pos? (value program c)) (pos? (value program m)))
-            (-> program
-                (update :pc + 6)
-                (assoc :regs (-> regs
-                                 (update a + (* (value program m) (value program c)))
-                                 (assoc b 0)
-                                 (assoc c 0))))
-            (update program :pc inc))
-          (if-let [[x y] (addition pc code)]
-            (if (pos? (value program y))
-              (-> program
-                  (update :pc + 3)
-                  (assoc :regs (-> regs
-                                   (update x + (value program y))
-                                   (assoc y 0))))
-              (update program :pc inc))
-            (let [[op x y] (code pc)]
-              (case op
-                :cpy (if (keyword? y)
-                       (-> program
-                           (update :pc inc)
-                           (assoc-in [:regs y] (value program x)))
-                       (update program :pc inc))
-                :inc (if (keyword? x)
-                       (-> program
-                           (update :pc inc)
-                           (update-in [:regs x] inc))
-                       (update program :pc inc))
-                :dec (if (keyword? x)
-                       (-> program
-                           (update :pc inc)
-                           (update-in [:regs x] dec))
-                       (update program :pc inc))
-                :jnz (update program :pc (if (zero? (value program x))
-                                           inc
-                                           #(+ (value program y) %)))
-                :tgl (-> program
-                         (update :pc inc)
-                         (assoc :code (toggle (+ pc (value program x)) code)))
-                :out (-> program
-                         (update :pc inc)
-                         (update :output conj (value program x)))))))))))
+      (let [val (fn [x] (get regs x x))
+            [op x y z w] (opt-code pc)]
+        (case op
+          :mul (if (and (pos? (val x)) (pos? (val w)))
+                 (-> program
+                     (update :pc + 6)
+                     (assoc :regs (-> regs
+                                      (update y + (* (val x) (val w)))
+                                      (assoc z 0)
+                                      (assoc w 0))))
+                 (update program :pc inc))
+          :add (if (pos? (val y))
+                 (-> program
+                     (update :pc + 3)
+                     (assoc :regs (-> regs
+                                      (update x + (val y))
+                                      (assoc y 0))))
+                 (update program :pc inc))
+          :div (if (pos? (val y))
+                 (-> program
+                     (update :pc + 8)
+                     (assoc :regs (-> regs
+                                      (update x + (quot (val y) 2))
+                                      (assoc z (- 2 (mod (val y) 2)))
+                                      (assoc y 0))))
+                 (update program :pc inc))
+          :nop (update program :pc inc)
+          :cpy (if (keyword? y)
+                 (-> program
+                     (update :pc inc)
+                     (assoc-in [:regs y] (val x)))
+                 (update program :pc inc))
+          :inc (if (keyword? x)
+                 (-> program
+                     (update :pc inc)
+                     (update-in [:regs x] inc))
+                 (update program :pc inc))
+          :dec (if (keyword? x)
+                 (-> program
+                     (update :pc inc)
+                     (update-in [:regs x] dec))
+                 (update program :pc inc))
+          :jnz (update program :pc (if (zero? (val x))
+                                     inc
+                                     #(+ (val y) %)))
+          :tgl (let [target (+ pc (val x))
+                     new-code (toggle target (:code program))]
+                 (-> program
+                     (update :pc inc)
+                     (assoc :code new-code)
+                     (assoc :opt-code (optimize new-code))))
+          :out (-> program
+                   (update :pc inc)
+                   (update :output conj (val x))))))))
 
 (defn execute [program]
   (loop [prog program]
