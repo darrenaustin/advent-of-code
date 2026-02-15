7adc0fff15c45f4ba2926947c8d40bacf48663b1
Darren Austin <darren@darrenaustin.org>
2026-02-14 17:49:08 -0800

    Cleaned up 2023.1

--- clojure/src/aoc2023/day01.clj
+++ clojure/src/aoc2023/day01.clj
@@ -2,42 +2,39 @@
 (ns aoc2023.day01
   (:require
    [aoc.day :as d]
-   [aoc.util.math :as m]
+   [aoc.util.math :as m :refer [sum]]
    [aoc.util.string :as s]
    [clojure.string :as str]))
 
 (defn input [] (d/day-input 2023 1))
 
-(def digit-names ["zero" "one" "two" "three" "four"
-                  "five" "six" "seven" "eight" "nine"])
+(def ^:private digit-names
+  ["zero" "one" "two" "three" "four" "five" "six" "seven" "eight" "nine"])
 
-(def word->digit
+(def ^:private word->digit
   (reduce-kv (fn [m i w] (assoc m w i (str i) i)) {} digit-names))
 
-(def word-digit-regex
-  (re-pattern (str/join "|" (conj digit-names #"\d"))))
+(def ^:private word-digit-regex
+  (re-pattern (str/join "|" (conj digit-names "\\d"))))
 
-(defn calibration-value [line]
+(defn- calibration-value [line]
   (->> (re-seq #"\d" line)
        ((juxt first last))
        str/join
        s/int))
 
-(defn word-calibration-value [line]
+(defn- word-calibration-value [line]
   (->> (s/re-seq-overlapping word-digit-regex line)
        ((juxt first last))
        (map word->digit)
        str/join
        s/int))
 
-(defn part1 [input]
-  (->> input
-       s/lines
-       (map calibration-value)
-       m/sum))
+(defn- calibration-sum [input calib-fn]
+  (->> (s/lines input)
+       (map calib-fn)
+       sum))
 
-(defn part2 [input]
-  (->> input
-       s/lines
-       (map word-calibration-value)
-       m/sum))
+(defn part1 [input] (calibration-sum input calibration-value))
+
+(defn part2 [input] (calibration-sum input word-calibration-value))
